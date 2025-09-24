5d0456dd8967a86d4843352bb05fda182417cdf7
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-24 09:40:40 -0300

    add sass support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'node:url'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  css: ['styles/main.sass', 'styles/reset.sass'],
+  alias: {
+  'images': fileURLToPath(new URL('./app/assets/images', import.meta.url)),
+  'styles': fileURLToPath(new URL('./app/assets/styles', import.meta.url)),
+  },
 })
+
