3dd6b0efa53adf30156e4788fcf92e0ec2f2c18f
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-24 09:40:13 -0300

    add eslint custom options

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+  },
+})
