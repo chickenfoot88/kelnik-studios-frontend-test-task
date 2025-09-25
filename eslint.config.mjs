30a8d8b07887098bfeebdbd5df30587b67e220e5
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-25 11:45:00 -0300

    remove semicolon in eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
   },
-})
+});
