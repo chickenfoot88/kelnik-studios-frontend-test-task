08754ea2c31988760615ac4299feaa98f2044c8d
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-26 07:30:09 -0300

    add button base component

---
+++ app/components/ui/ButtonBase.vue
@@ -0,0 +1,25 @@
+<template>
+  <button class="button-base" @click="event => emit('click', event)">
+    <slot/>
+  </button>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits<{
+  (e: 'click', event: MouseEvent): void
+}>()
+
+</script>
+
+<style scoped lang="sass">
+.button-base
+  cursor: pointer
+  font-size: rem(16px)
+  font-weight: 500
+  line-height: rem(24px)
+  padding: 8px 24px
+  color: $color-text-primary
+  background: $color-white
+  border-radius: 25px
+  border: 1px solid rgba($color-text-primary, 0.2)
+</style>
