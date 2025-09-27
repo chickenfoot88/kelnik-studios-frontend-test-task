af68fdbd1417d9f57d8898adc9eee62619804f56
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-27 09:59:07 -0300

    remove query params form apartments api call

--- app/composables/useApartments.ts
+++ app/composables/useApartments.ts
@@ -1,19 +1,18 @@
-import type { IApartment, IApartmentsQuery } from '~/types/apartments.type';
+import type { IApartment } from '~/types/apartments.type';
 
 export function useApartments() {
   const fetchApartments = async () => {
-    const params:IApartmentsQuery = {}
 
     const data = await $fetch<IApartment[]>(
       'mock/apartments.json',
       {
         method: 'GET',
-        params,
       },
     )
    
     return { data }
-  };
+    
+  }
 
   return { fetchApartments }
 }
