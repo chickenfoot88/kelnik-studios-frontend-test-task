f78b97e7cfa7489f12688b7981e3d5f40628c4de
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-28 07:30:23 -0300

    add disabled & loading state for base button

--- app/components/ui/ButtonBase.vue
+++ app/components/ui/ButtonBase.vue
@@ -1,10 +1,24 @@
 <template>
-  <button class="button-base" @click="event => emit('click', event)">
-    <slot/>
+  <button
+    :class="['button-base', { loading, disabled }]"
+    :disabled
+    @click="event => emit('click', event)"
+  >
+   <span class="button-base-content">
+     <div v-show="loading" class="button-base-spinner spinner"></div>
+     <span class="button-base-text">
+       <slot/>
+      </span>
+    </span>
   </button>
 </template>
 
 <script setup lang="ts">
+const { disabled = false, loading = false } = defineProps<{
+  loading?: boolean,
+  disabled?: boolean
+}>()
+
 const emit = defineEmits<{
   (e: 'click', event: MouseEvent): void
 }>()
@@ -13,6 +27,9 @@ const emit = defineEmits<{
 
 <style scoped lang="sass">
 .button-base
+  display: inline-flex
+  align-items: center
+  justify-content: center
   cursor: pointer
   font-size: rem(16px)
   font-weight: 500
@@ -23,10 +40,37 @@ const emit = defineEmits<{
   border-radius: 25px
   border: 1px solid rgba($color-text-primary, 0.2)
   transition: background $animation-duration ease
+  
+  &-content
+    display: inline-grid
+    place-items: center
 
-  &:hover
-    background: rgba($color-text-primary, 0.05)
+  &-text
+    opacity: 1
 
-  &:active
-    background: rgba($color-text-primary, 0.15)
+  &-spinner,
+  &-text
+    grid-area: 1 / 1
+    transition: opacity $animation-duration ease
+
+  &:not(:disabled)
+    &:hover
+      background: rgba($color-text-primary, 0.05)
+
+    &:active
+      background: rgba($color-text-primary, 0.15)
+
+    &.loading
+      cursor: default
+      pointer-events: none
+      opacity: 0.6
+          
+      .button-base-text
+        opacity: 0
+
+  &:disabled
+    color: $color-text-secondary
+    cursor: not-allowed
+    opacity: 0.5
+
 </style>
