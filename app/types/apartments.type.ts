d2e438a9665ab5d66fa45de97e395adeaa4458d2
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-28 12:22:46 -0300

    add IApartmentsQuery interface

--- app/types/apartments.type.ts
+++ app/types/apartments.type.ts
@@ -1,4 +1,4 @@
-export interface IApartment {
+interface IApartment {
   imgUrl: string
   imgAlt: string
   title: string
@@ -7,4 +7,16 @@ export interface IApartment {
   price: number
 }
 
-export type IApartmentWithFormattedPrice = Omit<IApartment, 'price'> & { price: string }
+type IApartmentWithFormatted = Omit<IApartment, 'price'> & { price: string }
+
+interface IApartmentsQuery {
+  limit: number
+  sortBy: keyof Pick<IApartment, 'price' | 'area' | 'level'>
+  sortOrder: 'asc' | 'desc'
+  priceMin: number
+  priceMax: number
+  areaMin: number
+  areaMax: number
+}
+
+export type { IApartment, IApartmentWithFormatted, IApartmentsQuery }
