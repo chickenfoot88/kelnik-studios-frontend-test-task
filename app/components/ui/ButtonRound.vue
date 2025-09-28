e25a6b254f8ec5364d90d88ac3ceee5b16f9dcae
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-28 10:46:24 -0300

    add ButtonRound

---
+++ app/components/ui/ButtonRound.vue
@@ -0,0 +1,22 @@
+<template>
+  <ButtonBase class="button-round" :style>
+    <slot/>
+  </ButtonBase>
+</template>
+
+<script setup lang="ts">
+import ButtonBase from './ButtonBase.vue'
+
+const { size = 40 } = defineProps<{ size?: number}>()
+
+const style = computed(() => ({
+  width: `${size}px`,
+  height: `${size}px`,
+}))
+
+</script>
+
+<style scoped lang="sass">
+.button-round
+  padding: 8px
+</style>
