3622213b0944feca4119e92dcb2de9e236a197da
chickenfoot88 <chikenfoot88@gmail.com>
2025-09-30 14:15:51 -0300

    update apartments store

--- app/stores/apartments.store.ts
+++ app/stores/apartments.store.ts
@@ -1,25 +1,24 @@
 import { computed } from 'vue'
 import { defineStore } from 'pinia'
-import type { IApartment, IApartmentWithFormatted, IApartmentsQuery } from '~/types/apartments.type'
+import type { IApartment, IApartmentFormatted, IApartmentsQuery } from '~/types/apartments.type'
 import { formatter } from '~/utils/formatter'
 
 export const useApartmentsStore = defineStore('apartments', () => {
   const apartmentsList = ref<IApartment[]>([])
   const totalApartmentsLength = ref<number>(0)
   
-  const apartmentListComputed = computed<IApartmentWithFormatted[]>(() => formatApartmentsList(apartmentsList.value))
+  const apartmentListComputed = computed<IApartmentFormatted[]>(() => formatApartmentsList(apartmentsList.value))
 
-  function formatApartmentsList(apartmentsList: IApartment[]): IApartmentWithFormatted[] {
+  function formatApartmentsList(apartmentsList: IApartment[]): IApartmentFormatted[] {
     return apartmentsList.map(apartment => ({
       ...apartment,
       price: formatter.format(apartment.price),
+      area: formatter.format(apartment.area),
     }))
   }
 
   async function getApartments(filterParams: IApartmentsQuery) {
     const { data, total } = await useApartments().fetchApartments(filterParams)
-    console.log(data)
-    
     apartmentsList.value = data
     totalApartmentsLength.value = total
   }
