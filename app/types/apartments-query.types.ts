63cbdbca42b5ec1b569f669fd02cc11d65cf1c8a
chickenfoot88 <chikenfoot88@gmail.com>
2025-10-02 08:41:09 -0300

    extend IApartmentsQuery interface rooms property

--- app/types/apartments-query.types.ts
+++ app/types/apartments-query.types.ts
@@ -1,7 +1,8 @@
 import type { ISortParamsType } from '~/types/apartment-sort.types'
 import type { IFilterParamsType } from '~/types/apartments-filter.types'
 
-interface IApartmentsQuery extends ISortParamsType, IFilterParamsType {
+interface IApartmentsQuery extends Omit<IFilterParamsType, 'rooms'>, ISortParamsType {
+  rooms: number | '' | null
   limit: number
 }
 
