e1f3f4e3f420e3b9b6ba1247af054fc3e950d34d
chickenfoot88 <chikenfoot88@gmail.com>
2025-10-02 08:36:28 -0300

    add ButtonText component

---
+++ app/components/ui/ButtonText.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="button-text">
+    <slot name="label"/>
+    <div class="button-text-icon">
+      <slot name="icon" />
+    </div>
+  </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="sass">
+.button-text
+  color: $color-black
+  background: none
+  border: none
+  cursor: pointer
+  font-size: rem(14px)
+  padding: 0
+  display: flex
+  justify-content: space-between
+  gap: 6px
+  transition: color $animation-duration ease
+
+  &:hover
+    color: $color-success-primary
+    .button-text-icon
+      fill: $color-success-primary
+      
+.button-text-icon
+  transition: fill $animation-duration ease
+  fill: $color-black
+  
+</style>
