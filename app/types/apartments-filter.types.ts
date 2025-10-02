02abc6383964f756ef7ac18d908803551520d689
chickenfoot88 <chikenfoot88@gmail.com>
2025-10-02 08:40:41 -0300

    add filter constants

--- app/types/apartments-filter.types.ts
+++ app/types/apartments-filter.types.ts
@@ -6,4 +6,14 @@ interface IFilterParamsType {
   rooms: number
 }
 
-export type { IFilterParamsType }
+const APARTMENTS_ROOMS_OPTIONS = [1, 2, 3, 4]
+const PRICE_RANGE = {
+  MIN: 1000000,
+  MAX: 28000000,
+}
+const AREA_RANGE = {
+  MIN: 16,
+  MAX: 200,
+}
+
+export { APARTMENTS_ROOMS_OPTIONS, PRICE_RANGE, AREA_RANGE, type IFilterParamsType }
